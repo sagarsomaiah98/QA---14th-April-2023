51ecb60e09aa53ec3c6ccb69ebc65f61a5bde765
Som <somaiah1988@gmail.com>
2023-05-09 22:09:50 -0400

    04

---
+++ QA Class 18 Strings and Arrays in Java/src/com/janbask/ArrayExamples/Two_dim_array.java
@@ -0,0 +1,12 @@
+package com.janbask.ArrayExamples;
+
+public class Two_dim_array {
+
+	public static void main(String[] args) {
+		
+
+		
+
+	}
+
+}
---
+++ QA Class 18 Strings and Arrays in Java/src/com/janbask/StringfunctionExamples/String_FunctionsEx.java
@@ -0,0 +1,13 @@
+package com.janbask.StringfunctionExamples;
+
+public class String_FunctionsEx {
+
+	public static void main(String[] args) {
+	
+		String value1= "Welcome to Janbask";
+		
+		String msg=" your order is succesfull and order number is AE34200";
+
+	}
+
+}
