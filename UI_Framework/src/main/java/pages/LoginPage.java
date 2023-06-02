0f5a8686295c9efd6293f70e8ab2dde11c22001f
Som <somaiah1988@gmail.com>
2023-06-01 22:07:08 -0400

    selenium

--- UI_Framework/src/main/java/pages/LoginPage.java
+++ UI_Framework/src/main/java/pages/LoginPage.java
@@ -3,17 +3,32 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 import base.TestBase;
 
 public class LoginPage extends TestBase {
 	
+	 
 	
-	public WebElement username= driver.findElement(By.xpath("//input[@id='user-name']"));
-	public WebElement password= driver.findElement(By.xpath("//input[@id='password']"));
-	public WebElement login= driver.findElement(By.xpath("//input[@id='login-button']"));
+	  @FindBy(xpath="//input[@id='user-name']")
+		WebElement username;	  
+	  
+	  @FindBy(xpath="//input[@id='password']")
+			WebElement password;	
+	  
+	  @FindBy(xpath="//input[@id='login-button']")
+		WebElement login;	
 	
 	
+  public LoginPage() {
+	  
+	  PageFactory.initElements(driver, this);
+	  
+  }
+	  
+	  
 	
 	public void login(String uname, String pwd) {
 		
--- UI_Framework/src/test/java/testCases/LoginTest.java
+++ UI_Framework/src/test/java/testCases/LoginTest.java
@@ -8,29 +8,32 @@ import org.testng.annotations.Test;
 
 import base.TestBase;
 import pages.LoginPage;
+import util.TestUtil;
 
 public class LoginTest extends TestBase{
-	
+	LoginPage l;
 	
 	@BeforeMethod
 	public void init() throws IOException {
 		
 		 intialize();
-		
+		 l=new LoginPage();
 	}
 	
 		
 	@Test
 	public void verifyValidLogin() throws IOException {
-		LoginPage l=new LoginPage();
+		//LoginPage l=new LoginPage();
 		l.login("standard_user","secret_sauce");
+		TestUtil.Screenshot();
 			
 	}
 	
 	@Test
 	public void verifyInValidLogin() throws IOException {	
-		LoginPage l=new LoginPage();
+		//LoginPage l=new LoginPage();
 		l.login("standard_user","secret_sauce1");	
+		TestUtil.Screenshot();
 		
 	}
 	
